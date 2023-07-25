1cd374a961c6fddda45ca3d4005c58670b335e74
Joao Almeida <joaoallmeida96@gmail.com>
2023-07-25 01:31:35 +0000

    Updated deployment script

--- scripts/deployment_lambda.sh
+++ scripts/deployment_lambda.sh
@@ -4,22 +4,6 @@ function_name="cenipa-etl"
 aws_region="us-east-1"
 s3_bucket="cenipa.etl.com.br"
 
-# Building Local Environment
-# mkdir venv
-# python -m pip install --upgrade pip setuptools wheel
-# python -m venv venv
-# source venv/bin/activate
-# pip install -r requirements.txt
-# deactivate
-
-# Creating Zip File
-# cp -r venv/lib/python3.10/site-packages/. ./
-# zip -r -9 "lambda_deploy.zip" . -x '*venv*' '*.git*' '*infra*'
-
-# Print local folder and contents
-# pwd
-# ls -las
-
 
 # Creating functions for create or update Lambda Fucntion
 function_exists() {
@@ -33,7 +17,6 @@ update_function() {
     --function-name "$function_name" \
     --image-uri 400582553708.dkr.ecr.us-east-1.amazonaws.com/cenipa-etl:latest \
     --region "$aws_region"
-    # --zip-file=fileb://lambda_deploy.zip \
 }
 
 create_function() {
@@ -44,14 +27,9 @@ create_function() {
         --role arn:aws:iam::400582553708:role/service-role/cenipa-etl-role-o11blh01 \
         --timeout 600 \
         --memory-size 512 \
-        --vpc-config '{"SubnetIds": ["subnet-094736222d17ac479", "subnet-0b2b1239195367b1a"], "SecurityGroupIds": ["sg-0ebd6e8c7c844104d"]}' \
         --environment Variables="{s3_bucket=$s3_bucket}" \
         --code ImageUri=400582553708.dkr.ecr.us-east-1.amazonaws.com/cenipa-etl:latest \
         --region "$aws_region"
-        # --layers arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python310:3 \
-        # --runtime python3.10 \
-        # --handler lambda_function.lambda_handler \
-        # --zip-file=fileb://lambda_deploy.zip \
 }
 
 if function_exists; then
